c9a16ec113672bd0be747106f42ca26329ec5c4e
Thirukkumaran <thirukkumaran@aravind.org>
2023-03-22 18:05:08 +0530

    After solid principles applied

--- VideoRentalStore/Models/Video.cs
+++ VideoRentalStore/Models/Video.cs
@@ -7,10 +7,28 @@ namespace VideoRentalStore.Models
     public class Video
     {
         public string Title { get; set; }
-        public bool CheckedOut { get; set; }
         public double AverageRating { get; set; }
 
         private List<int> Ratings = new List<int>();
+
+        public void ReceiveRating(int rating)
+        {
+            Ratings.Add(rating);
+            AverageRating = Ratings.Average();
+        }
+    }
+    public interface IRentable
+    {
+        void CheckOut();
+        void Return();
+    }
+
+    public class VideoRent : IRentable
+    {
+        public Video Video { get; set; }
+
+        public VideoRent videoRent { get; set; }
+        public bool CheckedOut { get; set; } = false;
 
         public void CheckOut()
         {
@@ -21,14 +39,5 @@ namespace VideoRentalStore.Models
         {
             CheckedOut = false;
         }
-
-        public void ReceiveRating(int rating)
-        {
-            Ratings.Add(rating);
-            AverageRating = Ratings.Average();
-        }
-
-
-
     }
 }
